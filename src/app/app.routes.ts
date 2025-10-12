0f217acba20a6372b0cd0c072cf4d8ae20737874
Brian <leonelhidalgo2206@gmail.com>
2025-10-12 14:35:52 -0300

    diseño de vista home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+// En app.routes.ts
 import { Routes } from '@angular/router';
+// Asegúrate de que el nombre aquí coincida con la clase exportada
+import { HomeComponent } from './home/home.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent } // Usa el nombre correcto
+];
